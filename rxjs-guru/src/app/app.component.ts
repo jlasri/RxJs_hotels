918a15aa322a720674ac9c19f1a77bcd46cd1da0
Jamal LASRI <jamallasri.pro@gmail.com>
2022-07-15 16:29:03 +0200

    First commit

---
+++ rxjs-guru/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, interval, Subscription, of, from } from 'rxjs';
+import { map, tap, take, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription = new Subscription();
+
+  ngOnInit() {
+
+
+
+    from([1, 2, 0, undefined, 12, 0, 14, 0, 15])
+      .pipe(
+        // tap(elem => console.log(elem)),
+        // map((elem: number) => {
+        //   if (elem === 0) {
+        //     throw new Error('zero erreur')
+        //   }
+
+        //   return elem * 2;
+        // }),
+        // map(item => item - 2),
+        // take(2)
+        filter(elem => elem !== undefined && elem != 0)
+      )
+      .subscribe(
+        (item: number | undefined) => console.log(`ma valeur ${item}`),
+        (err: unknown) => console.error(err),
+        () => console.log('Termine')
+      )
+
+
+  }
+
+
+  public start(): void {
+    this.subscription.add(interval(1000).subscribe(
+      value => console.log('ma valeur: ', value),
+      error => console.error(error),
+      () => console.log('terminé')
+    ))
+
+    this.subscription.add(interval(1000).subscribe(
+      value => console.warn('=== ma valeur === : ', value),
+      error => console.error(error),
+      () => console.warn('=== terminé ===')
+    ))
+  }
+
+  public stop(): void {
+    this.subscription.unsubscribe();
+
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
